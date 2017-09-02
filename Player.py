9b5b9f0a24dcb730e64068226c4e30172b677a87
AAA <aaa@aaa.com>
2017-09-02 18:18:30 -0500

    Added player class

---
+++ Player.py
@@ -0,0 +1,22 @@
+class Player(object):
+	
+	def __init__(self, name,hand):
+		self.name = name
+		self.hand = hand
+		self.pairs = []
+		
+	def find_pairs(self):
+		a=0
+		b=0
+		for i in self.hand.cards:
+			a=0
+			b=0
+			a+=1
+			for ii in self.hand.cards:
+				b+=1
+				if a != b:
+					if i.rank == ii.rank:
+						self.pairs.append(i)
+						self.hand.cards.remove(i)
+						self.hand.cards.remove(ii)
+						
