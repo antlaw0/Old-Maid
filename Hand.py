88981afca87fa7739478c290695fda7698a7c6fe
MF <notsure@compuserv.com>
2017-09-05 18:41:38 -0500

    made alternative pairing method

--- Hand.py
+++ Hand.py
@@ -2,6 +2,7 @@ class Hand(object):
     # "A hand of playing cards"
     def __init__(self):
         self.cards = []
+
     def __str__(self):
         if self.cards:
             playingCards = ""
@@ -10,13 +11,15 @@ class Hand(object):
         else:
             playingCards = "There is no cards left to be played"
         return playingCards
+
     def clear(self):
         ## Im clearing all cards from hand
         self.cards = []
+
     def adding(self, card):
         self.cards.append(card)
 
     def giving(self, card, otherHand):
         # A distributing method
+        otherHand.append(card)
         self.cards.remove(card)
-        otherHand.add(card)
--- Player.py
+++ Player.py
@@ -1,22 +1,32 @@
 class Player(object):
 
-	def __init__(self, name,hand):
-		self.name = name
-		self.hand = hand
-		self.pairs = []
+    def __init__(self, name,hand):
+        self.name = name
+        self.hand = hand
+        self.pairs = []
 
-	def find_pairs(self):
-		a=0
-		b=0
-		for i in self.hand.cards:
-			a=0
-			b=0
-			a+=1
-			for ii in self.hand.cards:
-				b+=1
-				if a != b:
-					if i.rank == ii.rank:
-						self.pairs.append(i)
-						self.hand.cards.remove(i)
-						self.hand.cards.remove(ii)
+    def find_pairs(self):
+        for card in self.hand:
+            for card2 in self.hand:
+                if card != card2:
+                    if card.rank == card2.rank:
+                        pair = [card, card2]
+                        self.pairs.append(pair)
+                        self.hand.cards.remove(card)
+                        self.hand.cards.remove(card2)
+
+
+                # a=0
+                # b=0
+                # for i in self.hand.cards:
+                # 	a=0
+                # 	b=0
+                # 	a+=1
+                # 	for ii in self.hand.cards:
+                # 		b+=1
+                # 		if a != b:
+                # 			if i.rank == ii.rank:
+                # 				self.pairs.append(i)
+                # 				self.hand.cards.remove(i)
+                # 				self.hand.cards.remove(ii)
 						
