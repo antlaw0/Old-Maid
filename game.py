d545e71d419bfdef28d449442d1c3303e812ad3a
MF <notsure@compuserv.com>
2017-09-06 09:08:05 -0500

    made game functions for making players and dealing cards

--- game.py
+++ game.py
@@ -7,16 +7,18 @@ import Computer
 def main():
     deck = Deck.Deck()
     print(deck)
-    drawnCard = deck.deal(5)
-    print(drawnCard)
+    # drawnCard = deck.deal(5)
+    # print(drawnCard)
+    allPlayers = []
 
-hHand = Hand.Hand()
-cHand = Hand.Hand()
+    allPlayers = makePlayer(1, allPlayers)
+    allPlayers = makePlayer(2, allPlayers)
+    dealOutCards(allPlayers, deck)
 
 # deck.deal([hHand, cHand], 20)
 #
-# human = Player.Player("Human", hHand)
-# computer = Player.Player("AI", cHand)
+#
+#
 # #print(computer.hand)
 # #print(human.hand)
 # loop = True
@@ -29,5 +31,20 @@ cHand = Hand.Hand()
 # print(human.hand)
 
 #while(loop == True):
-	
+
+def makePlayer(playerType, allPlayers):
+    hand = Hand.Hand()
+    if playerType == 1:
+# TODO replace name with input.
+        player = Player.Player("Human", hand)
+    elif playerType == 2:
+        player = Computer.Computer("Computer", hand)
+    allPlayers.append(player)
+    return allPlayers
+
+def dealOutCards(players, deck):
+    print("deck:" + str(len(deck.deck)) + " players:" + str(len(players)))
+    cardsPerPlayer = int(len(deck.deck) / len(players))
+    print(cardsPerPlayer)
+    extraCards = len(deck.deck) % len(players)
 main()
